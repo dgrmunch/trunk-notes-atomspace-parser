f3fd4c184b0b2c1e29c24d4572161d028cc3a3cd
Diego Gonzalez-Rodriguez <xmunch@xmunch.com>
2015-02-01 18:36:18 +0100

    Create AppDescription to setup environment

---
+++ src/main/java/com/xmunch/trunk_notes_atomspace_parser/AppDescription.java
@@ -0,0 +1,27 @@
+package com.xmunch.trunk_notes_atomspace_parser;
+
+public class AppDescription {
+
+	public static final Integer SUCCESS = 200;
+	public static final String ACCEPT = "accept";
+	public static final String SERVER = "http://localhost:8080/";
+	public static final String CREATE_VERTEX = "vertex/create/?";
+	public static final String CREATE_EDGE = "edge/create/?";
+	public static final String INPUT_FOLDER = "/Users/xmunch/Dropbox/trunksync/notes";
+	public static final String JSON = "application/json";
+	public static final String ERROR = "Failed : HTTP error code : ";
+	public static final String EMPTY = "";
+	public static final String SPACE = " ";
+	public static final String LABEL = "label";
+	public static final String EQUALS = "=";
+	public static final String AND = "&";
+	public static final String TYPE = "type";
+	public static final String TO = "to";
+	public static final String FROM = "from";
+	public static final Object IS_A = "is_a";
+	public static final String COLON = ":";
+	public static final String COMMA = ",";
+	public static final String PARAMS = "params";
+	public static final String PARAM_PROBLEM = "There was a problem parsing the param: ";
+	
+}
